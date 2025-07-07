e2cdde55cc13932d198ce516c521634e45cfb85c
Suman0703 <sumandevimehmi36@gmail.com>
2025-07-07 19:50:37 +0530

    next Day work

---
+++ Day_6/Class_work/index.js
@@ -0,0 +1,19 @@
+console.log(url);
+var url="https://www.google.com/search?q=total+ep+in+black+clover&oq=&gs_lcrp=EgZjaHJvbWUqCQgCEEUYOxjCAzIJCAAQRRg7GMIDMgkIARBFGDsYwgMyCQgCEEUYOxjCAzIJCAMQRRg7GMIDMgkIBBBFGDsYwgMyCQgFEEUYOxjCAzIJCAYQRRg7GMIDMgkIBxBFGDsYwgPSAQkzMTE0ajBqMTWoAgiwAgHxBQTG-KVCpoeP8QUExvilQqaHjw&sourceid=chrome&ie=UTF-8"
+myfun()
+console.log(url);
+console.log(b);
+const value=90;
+console.log(value);
+var b=8;
+console.log(b);
+function myfun(){
+    let a=10;
+    let b=40;
+    console.log(a+b);
+}
+console.log(url);
+myfun()
+
+
+
---
+++ Day_6/Practice/pr1.js
@@ -0,0 +1,71 @@
+console.log(document);
+document.getElementById("heading").innerText = "hi javascript";
+document.getElementById("heading").style.border = "1px solid blue";
+document.getElementById("heading").style.width = "300px";
+document.getElementById("heading").style.textAlign = "center";
+document.getElementById("H2").style.color = "pink";
+document.getElementById("h3").style.color = "Darkgreen";
+
+//variables
+//let, var, const
+let sam = 1;
+const samriti = 10;
+var muskan = 2;
+var muskan = 45;
+//let and const variables cannot be re-assigned
+console.log(sam);
+console.log(samriti);
+console.log(muskan);
+
+//Function
+function myfun() {
+    let firstName = "Suman";
+    let lastname = "devi";
+    console.log(firstName, lastname);
+    function nestedfun() {
+        let a = 10;
+        let b = 40;
+        console.log(a + b);
+    }
+    nestedfun();
+}
+myfun();
+
+//Execution of JS
+myfunction();
+let a = 10;
+console.log(a);
+console.log(b);
+var b = 50;
+const c = 30;
+console.log(c);
+function myfunction() {
+    console.log("hi from fun");
+}
+//Function Parameter 
+function fun(x, y) {
+    console.log(x + y);
+}
+let r = 30;
+let z = 40;
+fun(r, z);
+//concatenation
+function can(x, y) {
+    console.log(x + " " + y);
+    console.log(x, y);
+}
+let Q = "Rayat";
+let s = "Bahra";
+can(Q, s);
+
+//String 
+let name = "Rayat Bahra";
+let lenght = name.length;
+console.log(lenght);
+
+let firstchar = name[1];
+let lastchar = name[lenght - 5];
+console.log(lastchar);
+let splitChar = name.split("");
+console.log(splitChar);
+console.log(firstchar);
