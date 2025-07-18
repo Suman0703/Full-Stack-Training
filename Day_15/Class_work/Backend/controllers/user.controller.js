1c8b8d74fdd4529ad053f7eede7fa16218a27de2
Suman0703 <sumandevimehmi36@gmail.com>
2025-07-18 21:35:33 +0530

    practice work

--- Day_15/Class_work/Backend/controllers/user.controller.js
+++ Day_15/Class_work/Backend/controllers/user.controller.js
@@ -2,45 +2,48 @@ const { userModel } = require("../models/user.model");
 const bcrypt = require("bcrypt");
 
 
-const userSignup =async(req,res)=>{
-    const {name, email , password, phoneNumber, age} = req.body;
+const userSignup = async (req, res) => {
+    const { name, email, password, phoneNumber, age } = req.body;
     console.log(req.body);
-    
 
-    const userExist = await userModel.findOne({email});
+    // This checks if a user already exists in the database with the same email.
+    // It's a read operation using Mongoose.
+    const userExist = await userModel.findOne({ email });
 
-    if(userExist){
+    if (userExist) {
         return res.status(400).send("User already exists!");
     }
     
-
     try {
-        bcrypt.hash(password,7,async(err, hash)=>{
+        //Hash the password and save user
+        bcrypt.hash(password, 7, async (err, hash) => {
             if (err) {
                 return res.status(400).send("Error while hashing!");
             } else {
-                const user = new userModel({name, email, password:hash, phoneNumber, age});
-
+                const user = new userModel({ name, email, password: hash, phoneNumber, age });
+                // This saves the new user object to MongoDB.
                 await user.save();
                 res.status(200).send("Userdata successfully stored in DB");
             }
         });
-        
+
     } catch (error) {
-        res.status(400).send({error:error});
+        res.status(400).send({ error: error });
     }
 }
 
 
 
-const userLogin =()=>{
+const userLogin = () => {
 
 }
+
 
 module.exports = {
     userSignup
-    
+
 }
+
 
 
 
