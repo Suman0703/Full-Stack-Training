ba24d2ad4117e64768d4187efde583867c634e22
Suman0703 <sumandevimehmi36@gmail.com>
2025-07-22 18:02:36 +0530

    Delete Day_16 directory

--- Day_16/Class_work/Backend/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connection = mongoose.connect("mongodb+srv://sumandevimehmi36:sumandevi@cluster0.vukt4os.mongodb.net/myfirstDB?retryWrites=true&w=majority&appName=Cluster0 ");
-
-//exporting this file into index.js
-//curly braces to move move more than 1 module
-module.exports={
-    connection
-}
